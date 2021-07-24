d4b9cf3cba21682e6d32e43f20eff7da128a02eb
Jared Alford <jalford@springbuk.comn>
2021-07-24 14:37:43 -0400

    generate slug

--- lib/url_shortener/data/short_url.ex
+++ lib/url_shortener/data/short_url.ex
@@ -12,9 +12,18 @@ defmodule UrlShortener.Data.ShortUrl do
   @doc false
   def changeset(short_url, attrs) do
     short_url
-    |> cast(attrs, [:url, :slug])
-    |> validate_required([:url, :slug])
+    |> cast(attrs, [:url])
+    |> validate_required([:url])
+    |> validate_length(:url, min: 10, max: 2000)
     |> unique_constraint(:url)
     |> unique_constraint(:slug)
+    |> put_change(:slug, get_slug_hash(attrs["url"]))
+  end
+
+  defp get_slug_hash(url) do
+    case url do
+      nil -> nil
+      default -> :crypto.hash(:sha, default) |> Base.encode32() |> String.slice(0..7)
+    end
   end
 end
