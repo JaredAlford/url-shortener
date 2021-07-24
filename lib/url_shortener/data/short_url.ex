cd5e44ce28585d1cad4d75df21c644f6adb7ea81
Jared Alford <jalford@springbuk.comn>
2021-07-24 15:15:09 -0400

    url validation

--- lib/url_shortener/data/short_url.ex
+++ lib/url_shortener/data/short_url.ex
@@ -14,10 +14,28 @@ defmodule UrlShortener.Data.ShortUrl do
     short_url
     |> cast(attrs, [:url])
     |> validate_required([:url])
-    |> validate_length(:url, min: 10, max: 2000)
+    |> validate_url(:url)
     |> unique_constraint(:url)
     |> unique_constraint(:slug)
     |> put_change(:slug, get_slug_hash(attrs["url"]))
+  end
+
+  defp validate_url(changeset, field, options \\ []) do
+    validate_change(changeset, field, fn _, url ->
+      uri = URI.parse(url)
+
+      host_pieces =
+        case uri.host do
+          nil -> []
+          _something -> uri.host |> String.split(".")
+        end
+
+      case (uri.scheme == "http" or uri.scheme == "https") and Enum.count(host_pieces) > 1 and
+             not Enum.any?(host_pieces, fn piece -> piece == "" end) do
+        true -> []
+        false -> [{field, options[:message] || "Invalid URL"}]
+      end
+    end)
   end
 
   defp get_slug_hash(url) do
